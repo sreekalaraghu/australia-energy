beb814d4ba7ec493d69bc93ecad6da727744a951
ahyd0004 <ahyd0004@student.monash.edu>
2022-12-21 04:28:00 +1100

    Navbar-Smooth_scroll-Pages DONE

---
+++ src/components/handling/Handling.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './HandlingStyles.css'
+
+const Handling = () => {
+    return (
+        <div name='handling' className='handling'>
+            <div className="container">
+                <div className="top">
+                    <h1>StoryPanel</h1>
+                </div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Handling
---
+++ src/components/options/Options.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './OptionsStyles.css'
+
+const Options = () => {
+    return (
+        <div name='options' className='options'>
+            <div className="container">
+                <div className="top">
+                    <h1>Explore</h1>
+                </div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Options
